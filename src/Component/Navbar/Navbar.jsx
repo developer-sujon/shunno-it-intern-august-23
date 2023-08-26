9825cb274c669581fe9b606dbcf7e8efd5f4bd5a
Your Name <you@example.com>
2023-08-26 17:15:22 +0600

    navbar-section

--- src/Component/Navbar/Navbar.jsx
+++ src/Component/Navbar/Navbar.jsx
@@ -1,20 +1,41 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import classes from './Navbar.module.css'
+import React from "react";
+import {Link} from "react-router-dom";
+import classes from "./Navbar.module.css";
 
 const Navbar = () => {
-    return (
-        <div>
-            <h1 className='mb-3 mt-5'>Update Profile</h1>
-            <div>
-            <Link className='text-decoration-none fs-5'><a className={classes.text_1}  href="">Personal Info</a></Link>
-            <Link className='text-decoration-none'><a className={classes.text_1} href="">Educaton/Training</a></Link>
-            <Link className='text-decoration-none'><a className={classes.text_1} href="">Employment</a></Link>
-            <Link className='text-decoration-none'><a className={classes.text_1} href="">Other Information</a></Link>
-            <Link className='text-decoration-none'><a className={classes.text_1} href="">Photograph</a></Link>
-            </div>
-        </div>
-    );
+  return (
+    <div>
+      <h1 className="mb-3 mt-5">Update Profile</h1>
+      <div>
+        <Link className="text-decoration-none fs-6">
+          <a className={classes.text_1} href="">
+            Personal Info
+          </a>
+        </Link>
+        <Link className="text-decoration-none fs-6">
+          <a className={classes.text_1} href="">
+            Educaton/Training
+          </a>
+        </Link>
+        <Link className="text-decoration-none fs-6">
+          <a className={classes.text_1} href="">
+            Employment
+          </a>
+        </Link>
+        <Link className="text-decoration-none fs-6">
+          <a className={classes.text_1} href="">
+            Other Information
+          </a>
+        </Link>
+        <Link className="text-decoration-none fs-6">
+          <a className={classes.text_1} href="">
+            Photograph
+          </a>
+        </Link>
+      </div>
+      <hr className="mt-4" />
+    </div>
+  );
 };
 
 export default Navbar;
