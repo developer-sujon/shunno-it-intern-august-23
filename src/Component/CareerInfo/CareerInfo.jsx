3c70dc427faee6c0bfc0a300b60db56b7ab83c24
Bayshakhe Pramanik <bayshakhepramanik@gmail.com>
2023-08-27 16:33:08 +0600

    added career info accordion

--- src/Component/CareerInfo/CareerInfo.jsx
+++ src/Component/CareerInfo/CareerInfo.jsx
@@ -1,14 +1,24 @@
-import React from "react";
+import React, {useState} from "react";
 import { Accordion } from "react-bootstrap";
 import { useForm } from "react-hook-form";
 import { FaLink } from "react-icons/fa";
 import { MdFormatListNumbered } from "react-icons/md";
 import { PiListBulletsBold } from "react-icons/pi";
 import { RxCross2 } from "react-icons/rx";
+import Editor from "./Editor";
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
 
 const CareerInfo = () => {
-  const { register, handleSubmit } = useForm();
+  const [value, setValue] = useState('');
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
   const onSubmit = (data) => console.log(data);
+  // console.log(value)
   return (
     <div className="my-3">
       <Accordion defaultActiveKey="0">
@@ -17,8 +27,12 @@ const CareerInfo = () => {
             Career and Application Information
           </Accordion.Header>
           <Accordion.Body>
-            <label htmlFor="careerObjective" className="text-black">Career Objective <span className="text-danger">*</span></label>
-            <div className="border mt-2">
+            <form onSubmit={onSubmit} action="">
+              {/* Career objective */}
+            <label htmlFor="careerObjective" className="text-black">
+              Career Objective <span className="text-danger">*</span>
+            </label>
+            {/* <div className="border mt-2">
               <div className="d-flex gap-3 align-items-center p-3 border-bottom">
                 <select {...register("textSize")}>
                   <option value="normal">Normal</option>
@@ -43,7 +57,63 @@ const CareerInfo = () => {
                 id="careerObjective"
                 className="w-100 border-0 p-3"
               ></textarea>
+            </div> */}
+            <ReactQuill theme="snow" value={value} onChange={setValue} />
+
+            {/* Present Salary */}
+            <div className="my-2 row g-5">
+              <div className="col-12 col-lg-6 ">
+              <label htmlFor="presentSalary" className="text-black mb-2">
+              Present Salary <span className="text-danger">*</span>
+            </label>
+                <input
+                  {...register("presentSalary", { required: "Present salary is required" })}
+                  type="number"
+                  id="presentSalary"
+                  className="form-control"
+                  placeholder="Present Salary"
+                />
+                {errors?.presentSalary && (
+                  <span className="text-danger fw-bold">
+                    {errors?.presentSalary.message}
+                  </span>
+                )}
+                <p>TK/Month</p>
+                <label htmlFor="jobLevel" className="mt-1">Looking for (Job Level)</label><br />
+                <input type="radio" name="jobLevel" value="Entry Level"/> Entry Level
+                <input type="radio" name="jobLevel" value="Mid Level"/> Mid Level
+                <input type="radio" name="jobLevel" value="Top Level"/> Top Level
+              </div>
+
+              {/* Expected salary */}
+              <div className="col-12 col-lg-6">
+              <label htmlFor="expectedSalary" className="text-black mb-2">
+              Expected Salary <span className="text-danger">*</span>
+            </label>
+                <input
+                  {...register("expectedSalary", { required: "Expected salary is required" })}
+                  type="number"
+                  id="expectedSalary"
+                  className="form-control"
+                  placeholder="Expected Salary"
+                />
+                {errors?.expectedSalary && (
+                  <span className="text-danger fw-bold">
+                    {errors?.expectedSalary.message}
+                  </span>
+                )}
+                <p>TK/Month</p>
+                <label htmlFor="jobLevel" className="mt-1">Available for (Job Nature)</label><br />
+                <input type="radio" name="jobLevel" value="Full Time"/> Full time
+                <input type="radio" name="jobLevel" value="Part Time"/> Part time
+                <input type="radio" name="jobLevel" value="Contract"/> Contract
+                <input type="radio" name="jobLevel" value="Internship"/> Internship
+                <input type="radio" name="jobLevel" value="Freelance"/> Freelance
+              </div>
             </div>
+            </form>
+
+            {/* <Editor></Editor> */}
           </Accordion.Body>
         </Accordion.Item>
       </Accordion>
