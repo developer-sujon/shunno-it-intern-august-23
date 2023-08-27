8b7d71c76a2a55f8a0875f2695dc00a2813e7395
myself-rasel-mahmud <md.rasel.mahmud.mail@gmail.com>
2023-08-27 16:16:20 +0600

    change hello

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,13 +4,9 @@ const Home = () => {
   return (
     <div>
       {/* <ProfileIndex /> */}
-<<<<<<< HEAD
-      <h2>hello from rasel</h2>
-=======
       <h1>Hello Home Page 1</h1>
       <h1>Hello Home Page 2</h1>
       <h1>Hello Home Page 3</h1>
->>>>>>> 5edf46c6869066f53211fa820a6d5710c8201025
     </div>
   );
 };
