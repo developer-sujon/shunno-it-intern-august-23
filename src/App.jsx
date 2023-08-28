1a2d1c8047646f15d569366199ad94ac19e5c238
Bayshakhe Pramanik <bayshakhepramanik@gmail.com>
2023-08-28 18:28:54 +0600

    completed the others information page layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,7 @@
 import {} from "react";
 import { RouterProvider } from "react-router-dom";
-import { route } from "./Route/Route";
-<<<<<<< HEAD
-import "bootstrap/dist/css/bootstrap.min.css";
-=======
-import 'bootstrap/dist/css/bootstrap.min.css';
->>>>>>> 3f9b8b9e0c534872a43e546aad431b27faf63d4d
+import { route } from "./Route/Route"
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
   return (
