5374c0bffaf611dd7fca57020afb73718d7a63d1
Your Name <you@example.com>
2023-08-29 13:00:03 +0600

    Depart seciton

--- src/Component/Other_info/Other_info.jsx
+++ src/Component/Other_info/Other_info.jsx
@@ -6,8 +6,8 @@ const OtherInfo = () => {
   const { control, register } = useForm();
   const { fields, append, prepend, remove, swap, move, insert } = useFieldArray(
     {
-      control, // control props comes from useForm (optional: if you are using FormContext)
-      name: "test", // unique name for your Field Array
+      control, 
+      name: "test",
     }
   );
   let indexNum = 0;
