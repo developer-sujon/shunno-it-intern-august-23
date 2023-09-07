38f7189d9473bae4c4b325a3e745f795f97ab138
Bayshakhe Pramanik <bayshakhepramanik@gmail.com>
2023-09-07 20:41:48 +0600

    active react tab style implement with DOM

--- src/pages/Career.jsx
+++ src/pages/Career.jsx
@@ -1,12 +1,21 @@
-import { Component } from "react";
+import { useEffect, useRef, useState } from "react";
 import { BsArrowUpRightCircle } from "react-icons/bs";
 import { FaClock, FaMapMarkerAlt } from "react-icons/fa";
+import { Link } from "react-router-dom";
+import { FcSearch } from "react-icons/fc";
 
-class Career extends Component {
-  componentDidMount() {
+const Career = () => {
+  const [filterJobOpportunity, setFilterJobOpportunity] = useState([]);
+  const [isActive, setIsActive] = useState(false);
+  let btnRef = useRef();
+  let [active, setActive] = useState("");
+
+  useEffect(() => {
     window.scroll(0, 0);
-  }
-  jobOpportunity = [
+    setFilterJobOpportunity(jobOpportunity);
+  }, []);
+
+  const jobOpportunity = [
     {
       position: "junior Front-end Developer",
       location: "remote",
@@ -14,6 +23,7 @@ class Career extends Component {
       category: "development",
       description:
         "we are looking for a junior Front-end Developer to join our team",
+      isActive: false,
     },
     {
       position: "marketing officer",
@@ -22,6 +32,7 @@ class Career extends Component {
       category: "marketing",
       description:
         "we are looking for a mid-level marketing officer to join our team",
+      isActive: false,
     },
     {
       position: "customer service",
@@ -30,6 +41,7 @@ class Career extends Component {
       category: "support",
       description:
         "we are looking for a customer service officer to join our team",
+      isActive: false,
     },
     {
       position: "back-end Developer",
@@ -38,6 +50,7 @@ class Career extends Component {
       category: "development",
       description:
         "we are looking for a expert back end developer to join our team",
+      isActive: false,
     },
     {
       position: "product designer",
@@ -46,86 +59,94 @@ class Career extends Component {
       category: "designer",
       description:
         "we are looking for a expert product designer to join our team",
+      isActive: false,
     },
   ];
-  render() {
-    return (
-      <section
-        className="pt-4"
-        style={{
-          background:
-            "url(https://www.shunnoit.com/assets/img/bg/home.png) no-repeat fixed center ",
-        }}
+
+  const handleTab = (e) => {
+    const target = e.target;
+
+    for(let item of document.querySelectorAll('.amar_active_class')){
+      item.className = ''
+    }
+    target.className= 'border-bottom-0 border-start-0 border-end-0 border-primary border-4 amar_active_class'
+
+  };
+
+  return (
+    <section
+      className="pt-4"
+      style={{
+        background:
+          "url(https://www.shunnoit.com/assets/img/bg/home.png) no-repeat fixed center",
+      }}
+    >
+      <div
+        style={{ minHeight: "100vh" }}
+        className="container d-flex mt-5 pt-5 flex-column  gap-2 position-relative pb-5"
       >
         <div
-          style={{ minHeight: "100vh" }}
-          className="container d-flex mt-5 pt-5 flex-column  gap-2 position-relative pb-5"
+          className="rounded p-3"
+          style={{
+            background: "rgba(255,255,255, .1)",
+            boxShadow: "0 0 10px rgba(0,0,0, .2)",
+            backdropFilter: "blur(30px)",
+          }}
         >
-          <div
-            className="rounded p-3"
-            style={{
-              background: "rgba(255,255,255, .1)",
-              boxShadow: "0 0 10px rgba(0,0,0, .2)",
-              backdropFilter: "blur(30px)",
-            }}
-          >
-            <div>
-              <div className="btn btn-success pe-none rounded-pill px-3">
-                We are Hiring.
-              </div>
+          <div>
+            <div className="btn btn-success pe-none rounded-pill px-3">
+              We are Hiring.
             </div>
-            <h2 className="fw-bold my-2">Be Part of our Mission</h2>
-            <p style={{ maxWidth: "700px" }}>
-              We are looking for a passionate people to join us on our mission.
-              we value flat hierarchies, clear communication, and full ownership
-              and responsibility.
-            </p>
-            <div
-              style={{ background: "rgba(0,0,0, .2)" }}
-              className="d-flex gap-3 overflow-auto p-3 rounded"
-            >
+          </div>
+          <h2 className="fw-bold my-2">Be Part of our Mission</h2>
+          <p style={{ maxWidth: "700px" }}>
+            We are looking for a passionate people to join us on our mission. we
+            value flat hierarchies, clear communication, and full ownership and
+            responsibility.
+          </p>
+          <div
+            style={{ background: "rgba(0,0,0, .2)" }}
+            className="d-flex gap-3 overflow-auto p-3 rounded"
+          >
+            {!filterJobOpportunity.length <= 0 && (
               <button
+                onClick={handleTab}
                 style={{ background: "rgba(255,255,255, .5)" }}
-                className="btn border-4 border-bottom-0 border-start-0 border-end-0 border-primary"
+                className="btn "
               >
                 View all
               </button>
+            )}
+            {jobOpportunity?.map((jobTab) => (
               <button
+                onClick={handleTab}
                 style={{ background: "rgba(255,255,255, .5)" }}
-                className="btn border-4 border-bottom-0 border-start-0 border-end-0 border-primary"
+                className="tabBtn"
+                ref={(el) => (btnRef = el)}
               >
-                Development
+                {jobTab.category}
               </button>
-              <button
-                style={{ background: "rgba(255,255,255, .5)" }}
-                className="btn border-4 border-bottom-0 border-start-0 border-end-0 border-primary"
-              >
-                Design
-              </button>
-              <button
-                style={{ background: "rgba(255,255,255, .5)" }}
-                className="btn border-4 border-bottom-0 border-start-0 border-end-0 border-primary"
-              >
-                Marketing
-              </button>
-              <button
-                style={{ background: "rgba(255,255,255, .5)" }}
-                className="btn border-4 border-bottom-0 border-start-0 border-end-0 border-primary"
-              >
-                Customer Service
-              </button>
-              <button
-                style={{ background: "rgba(255,255,255, .5)" }}
-                className="btn border-4 border-bottom-0 border-start-0 border-end-0 border-primary"
-              >
-                Operations
-              </button>
-            </div>
+            ))}
           </div>
-          <hr />
+        </div>
+        <hr />
 
-          {/* job Opportunity */}
-          {this.jobOpportunity.map((job, index) => (
+        {/* job Opportunity */}
+        {filterJobOpportunity.length <= 0 ? (
+          <div
+            style={{
+              background: "rgba(0,0,0, .1)",
+              boxShadow: "0 0 10px rgba(255,255,255, .2)",
+              backdropFilter: "blur(20px)",
+            }}
+            className="border-0 p-3 text-capitalize rounded"
+          >
+            <h2 className="text-danger">
+              <FcSearch /> No Job Opportunity available at this moment
+            </h2>
+          </div>
+        ) : (
+          filterJobOpportunity?.map((job, index) => (
             <div
               key={index}
               style={{
@@ -147,15 +168,15 @@ class Career extends Component {
                   </div>
                 </div>
               </div>
-              <button className="btn btn-primary rounded-pill">
+              <Link to={`/job-apply/`} className="btn btn-primary rounded-pill">
                 Apply <BsArrowUpRightCircle />
-              </button>
+              </Link>
             </div>
-          ))}
-        </div>
-      </section>
-    );
-  }
-}
+          ))
+        )}
+      </div>
+    </section>
+  );
+};
 
 export default Career;
